72f71b0907066a01e63e8436cadae2b63d13723f
Anders Evjen <evjand@gmail.com>
2020-05-01 02:24:39 +0200

    Add sound files

---
+++ public/precache-manifest.f54ebe57db67862882cca22a53440033.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09db5e5bae976bc51bc3529c9a40ba6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be869c499350a49442b9",
+    "url": "/static/js/2.b3117928.chunk.js"
+  },
+  {
+    "revision": "5cfd4162a804c73fbceb18fa6cb44b6f",
+    "url": "/static/js/2.b3117928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5263af9a4959d9bd4cb3",
+    "url": "/static/js/main.91814a0d.chunk.js"
+  },
+  {
+    "revision": "28c836c6390ca2244059",
+    "url": "/static/js/runtime-main.3ce8a40d.js"
+  },
+  {
+    "revision": "1fcf8c005347b14db3c335b3f7ad7f9d",
+    "url": "/static/media/accepted.1fcf8c00.wav"
+  },
+  {
+    "revision": "08953c16a4db60196754d673739a8323",
+    "url": "/static/media/decline.08953c16.wav"
+  },
+  {
+    "revision": "9a9c4d64ba29e660b86f72eec7ba694d",
+    "url": "/static/media/hit.9a9c4d64.wav"
+  },
+  {
+    "revision": "f7553dc9e9dd00b78d6805e441e92195",
+    "url": "/static/media/miss.f7553dc9.wav"
+  },
+  {
+    "revision": "f8d95f310603ed72018dff7400d1708e",
+    "url": "/static/media/notallowed.f8d95f31.wav"
+  },
+  {
+    "revision": "0cae592a703cc52405cc97fd7aa4f166",
+    "url": "/static/media/notification1.0cae592a.wav"
+  },
+  {
+    "revision": "0e9986fbf778975e3ae2dbcb1f229477",
+    "url": "/static/media/notification3.0e9986fb.wav"
+  }
+]);
